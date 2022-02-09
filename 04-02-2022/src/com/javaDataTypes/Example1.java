d6b68645aa1960dc43a3dbb8d79d60a469ac9da9
nikitha <nikitha@LAPTOP-4GAKUODQ>
2022-02-09 16:16:07 +0530

    java datatypes added to project

---
+++ 04-02-2022/src/com/javaDataTypes/Example1.java
@@ -0,0 +1,24 @@
+package com.javaDataTypes;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		/**
+		 * write a java program to print customer information(id,name,address)
+		 */
+		  int customerId = 1234;
+		  String customerName = "Nikitha";
+		  String customerAddress = "India";
+		  
+		  System.out.print("Customer Id :");
+		  System.out.println(customerId);
+		  
+		  System.out.print("Customer Name :");
+		  System.out.println(customerName);
+		  
+		  System.out.print("Customer Address :");
+		  System.out.println(customerAddress);
+
+	}
+
+}
